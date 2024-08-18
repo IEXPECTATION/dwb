44ee655c3a9b12b9fcccac77150c367741c8c315
wuch <wuchx19@gmail.com>
2024-08-18 23:26:10 +0800

    chore: Add the glfw.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,41 @@
+// clang-format off
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
+// clang-format on
 
 int main() {
-  std::cout << "Hello World!\n";
+  if (!glfwInit()) {
+    std::cerr << "Failed to initialize GLFW." << std::endl;
+    return -1;
+  }
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow *window = glfwCreateWindow(800, 600, "OpenGL Square", NULL, NULL);
+  if (!window) {
+    std::cerr << "Failed to create GLFW window." << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cerr << "Failed to initialize GLAD." << std::endl;
+    return -1;
+  }
+
+  while (!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+
   return 0;
 }
